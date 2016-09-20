481aaf48169c6a0ba2f1bda9e10e1beac591b023
dskrei <dskrei@users.noreply.github.com>
2016-09-19 17:44:19 -0700

    Add files via upload

---
+++ USDANutritionDB.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using IReach.Models;
+using IReach.Services;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IReach.Helpers;
+using SQLite.Net.Async;
+using IReach.Service;
+using SQLite.Net;
+
+[assembly: Dependency(typeof(USDAFoodService))]
+namespace IReach.Service
+{
+    public class USDAFoodService : IUsdaFoodService
+    {
+
+        private static readonly AsyncLock Locker = new AsyncLock ( );
+        private SQLiteAsyncConnection Database { get; } = DependencyService.Get<ISQLiteUsda> ( ).GetAsyncConnection ( );
+
+        public Task<IList<common_nutrient>> GetCommonGetCommonNutrients ( )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public Task<common_nutrient> GetCommonNutrient ( )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public async Task<IList<food_group>> GetFoodGroups ( )
+        {
+            using ( await Locker.LockAsync() )
+            {
+                var result = await Database.QueryAsync<food_group>("SELECT * FROM food_group"); 
+                return result;
+            }
+        }
+
+        public async Task<IList<food>> GetFoodsWithGroupId(int groupId)
+        {
+            using (await Locker.LockAsync())
+            {
+                Debug.WriteLine("Database GetFoodGroupID = {0}", groupId);
+                var result = await (from s in Database.Table<food>()
+                              orderby s.id
+                              where s.food_group_id == groupId
+                              select s).ToListAsync();
+                Debug.WriteLine("Result Count = {0}", result.Count);
+                 
+                return result;
+            } 
+        }
+
+        public Task<IList<food>> GetFoods ( )
+        {
+            throw new NotImplementedException ( );
+        }
+
+        public async Task<food> GetFoodWithID (int foodId)
+        {
+            Debug.WriteLine("Get food ID = {0}", foodId);
+
+            var food = await (from s in Database.Table<food>()
+                where s.id == foodId
+                select s).FirstOrDefaultAsync(); 
+
+            return food;
+        }
+
+        public async Task<IList<food>> SearchFoods ( string searchString)
+        {
+            Debug.WriteLine("Search string = {0}", searchString);
+            var words = (IEnumerable<string>) searchString.Split(' ').ToList(); 
+
+            const string SqlClause = "short_desc LIKE '%{0}%'";
+            words = words.Select(word => string.Format(SqlClause, word.Replace("'", "''")));
+            var joined = string.Join(" AND ", words.ToArray());
+            const string SqlQuery = "SELECT * FROM food WHERE {0}";
+
+            var tableSearch = await Database.QueryAsync<food>(string.Format(SqlQuery, joined)); 
+            Debug.WriteLine("Found Count = {0}", tableSearch.Count);
+
+           return tableSearch;
+        }
+
+        public async Task<IList<food>> SearchFoods(string searchString, int groupId)
+        {
+            Debug.WriteLine("Searching for food name = {0} with group id = {1}", searchString, groupId); 
+            var words = (IEnumerable<string>) searchString.Split(' ').ToList();
+
+            const string SqlClause = "short_desc LIKE '%{0}%'";
+            words = words.Select(word => string.Format(SqlClause, word.Replace("'", "''")));
+
+            var joined = string.Join(" AND ", words.ToArray()); 
+            const string SqlQuery = "SELECT * FROM food WHERE food_group_id = {0} AND {1}";
+
+            var foods = await Database.QueryAsync<food>(string.Format(SqlQuery, groupId, joined)); 
+
+            Debug.WriteLine("Food Count = {0}", foods.Count);
+
+            return foods;
+        }
+
+        public async Task<int> GetCalories(int foodId) //new function to return calories
+        {
+            Debug.WriteLine("Get calories for food ID {0}", foodId);
+            String qstring = "SELECT amount FROM nutrition WHERE food_id=" + foodId.ToString() + " AND nutrient_id=208;";
+            var calories = await Database.QueryAsync<nutrition>(qstring);
+            return (int)calories[0].amount;
+        }
+         
+    }
+}
---
+++ UsdaFoodItemViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IReach.Models;
+using IReach.Services;
+using MvvmHelpers;
+using Xamarin.Forms;
+using IReach;
+
+namespace IReach.ViewModels
+{
+    public class UsdaFoodItemViewModel : BaseViewModel
+    {
+       
+        private static IUsdaFoodService FoodService { get; } = DependencyService.Get<IUsdaFoodService>();
+        private food item;
+        public UsdaFoodItemViewModel(food item)
+        {
+            this.item = item;
+        }
+
+        public string FoodName
+        {
+            get { return item.short_desc; }
+        }
+
+        private int _calories;
+        public int Calories
+        {
+            get {
+                return App.NutritionDb.GetCalories(item.id); //cannot convert Task<int> to int
+                }
+            set { SetProperty(ref _calories, value); }
+        }
+
+        private int _servings;
+        public int Servings
+        {
+            get
+            {
+                if (_servings > 1)
+                    return 2;
+                return _servings;
+            }
+            set { SetProperty(ref _servings, value); }
+        }
+
+
+        public async void Save()
+        {
+            var foodEntry = new FoodItem();
+            foodEntry.Name = FoodName;
+            foodEntry.Calories = Calories;
+            foodEntry.Servings = Servings;
+             
+            App.Database.SaveItem(foodEntry); 
+
+        }
+    }
+}
